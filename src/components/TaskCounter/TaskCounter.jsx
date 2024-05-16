2bed4b8927ae7e57d2c2bac3f77504eaedde3f73
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-16 15:02:35 +0300

    upd

--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -1,10 +1,26 @@
+import { useSelector } from 'react-redux';
 import css from './TaskCounter.module.css';
+import { selectTasks } from '../../redux/todoSlice';
 
 export const TaskCounter = () => {
+  const tasks = useSelector(selectTasks);
+
+  const count = tasks.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1;
+      } else {
+        acc.active += 1;
+      }
+      return acc;
+    },
+    { completed: 0, active: 0 }
+  );
+
   return (
     <div>
-      <p className={css.text}>Active: </p>
-      <p className={css.text}>Completed: </p>
+      <p className={css.text}>Active:{count.active} </p>
+      <p className={css.text}>Completed: {count.completed} </p>
     </div>
   );
 };
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,12 +2,27 @@ import { useSelector } from 'react-redux';
 import { Task } from '../Task/Task';
 import css from './TaskList.module.css';
 import { selectTasks } from '../../redux/todoSlice';
+import { selectFilters } from '../../redux/filterSlice';
+
+const getChosenTasks = (tasks, filter) => {
+  switch (filter) {
+    case 'active':
+      return tasks.filter(task => !task.completed);
+    case 'completed':
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
 
 export const TaskList = () => {
   const tasks = useSelector(selectTasks);
+  const filter = useSelector(selectFilters);
+  const filteredTasks = getChosenTasks(tasks, filter);
+
   return (
     <ul className={css.list}>
-      {tasks.map(task => (
+      {filteredTasks.map(task => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
